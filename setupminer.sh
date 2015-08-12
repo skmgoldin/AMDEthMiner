8a31850ff1b352d56391fd0ee659107d4c0fd1bf
Mike Goldin <skmgoldin@gmail.com>
2015-08-11 21:05:06 -0400

    Build from package repos rather than source.

--- setupminer.sh
+++ setupminer.sh
@@ -22,29 +22,13 @@ echo "==============================="
 sleep 5
 
 # Add repositories and setup
-sudo add-apt-repository -y ppa:ethereum/ethereum
-sudo add-apt-repository -y ppa:ethereum/ethereum-qt
-sudo apt-get -y install language-pack-en-base
-sudo dpkg-reconfigure locales
-sudo apt-get -y install software-properties-common
-wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo add-apt-repository "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty main"
 sudo apt-get update
 sudo apt-get upgrade -y
-
-# Build Geth
-sudo apt-get install -y ethereum libgmp3-dev golang
-mkdir -p ~/go; echo "export GOPATH=$HOME/go" >> ~/.bashrc
-echo "export PATH=$PATH:$HOME/go/bin:/usr/local/go/bin" >> ~/.bashrc
-source ~/.bashrc
-git clone https://github.com/ethereum/go-ethereum
-cd go-ethereum; git checkout master; sudo make geth; cd ~
-
-# Build Eth
-sudo apt-get -y install build-essential git cmake libboost-all-dev libgmp-dev libleveldb-dev libminiupnpc-dev libreadline-dev libncurses5-dev libcurl4-openssl-dev libcryptopp-dev libjson-rpc-cpp-dev libmicrohttpd-dev libjsoncpp-dev libargtable2-dev llvm-3.8-dev libedit-dev mesa-common-dev ocl-icd-libopencl1 opencl-headers libgoogle-perftools-dev qtbase5-dev qt5-default qtdeclarative5-dev libqt5webkit5-dev libqt5webengine5-dev ocl-icd-dev libv8-dev
-git clone https://github.com/ethereum/cpp-ethereum
-cd cpp-ethereum; git checkout master
-mkdir build; cd build; sudo cmake .. -DGUI=0 -DETHASHCL=1; sudo make -j $(nproc)
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo add-apt-repository -y ppa:ethereum/ethereum-qt
+sudo apt-get update
+sudo apt-get install ethereum -y
+sudo apt-get install cpp-ethereum -y
 
 # Install graphics drivers
 sudo apt-get install -y ubuntu-drivers-common
@@ -59,6 +43,6 @@ sudo reboot
 
 # Run
 # cd ~/go-ethereum/build/bin/
-# nohup ./geth --rpc --etherbase "0x753562eea69cd2d342a9c07edd49b9753dc0f01e" --genesis ~/genesis.json > ~/geth.log &
+# nohup geth --rpc --etherbase "0x753562eea69cd2d342a9c07edd49b9753dc0f01e" --genesis ~/genesis.json > ~/geth.log &
 # cd ~/cpp-ethereum/build/ethminer
-# nohup sudo ./ethminer -G -t 4 > ~/ethminer.log &
+# nohup sudo ethminer -G -t 4 > ~/ethminer.log &
