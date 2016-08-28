ffbd1119a6abc4da8697ce473268186b9411ee17
Mike Goldin <mike.goldin@consensys.net>
2016-08-28 09:12:35 -0400

    Remove inline sudo

--- setupminer.sh
+++ setupminer.sh
@@ -22,21 +22,21 @@ echo "==============================="
 sleep 5
 
 # Add repositories and setup
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo add-apt-repository -y ppa:ethereum/ethereum
-sudo add-apt-repository -y ppa:ethereum/ethereum-qt
-sudo apt-get update
-sudo apt-get install ethereum -y
-sudo apt-get install cpp-ethereum -y
+apt-get update
+apt-get upgrade -y
+add-apt-repository -y ppa:ethereum/ethereum
+add-apt-repository -y ppa:ethereum/ethereum-qt
+apt-get update
+apt-get install ethereum -y
+apt-get install cpp-ethereum -y
 
 # Install graphics drivers
-sudo apt-get install -y ubuntu-drivers-common
-sudo apt-get install -y fglrx-updates
+apt-get install -y ubuntu-drivers-common
+apt-get install -y fglrx-updates
 
 # Reboot
-sudo apt-get update && sudo apt-get upgrade -y
-sudo reboot
+apt-get update && sudo apt-get upgrade -y
+reboot
 
 # Run
 # nohup geth --rpc --etherbase "0x753562eea69cd2d342a9c07edd49b9753dc0f01e" --genesis ~/genesis.json > ~/geth.log &
