349aba4b8368bc0ea114d4805ce34fc4853ace2d
Mike Goldin <mike.goldin@consensys.net>
2016-08-28 09:18:26 -0400

    Refactor for better automation, clarity

--- setupminer.sh
+++ setupminer.sh
@@ -1,19 +1,5 @@
-# Before running:
-# sudo apt-get update && sudo apt-get upgrade -y
-# sudo apt-get install -y git openssh-server
-# sudo vim /etc/sudoers
-# Add the line:
-# <username> ALL=(ALL) NOPASSWD: ALL
-
-# Add graphics drivers repos
-# sudo vim /etc/apt/sources.list
-# add line:
-# deb http://archive.ubuntu.com/ubuntu/ trusty-proposed restricted main multiverse universe
-# sudo vim /etc/apt/preferences.d/proposed-updates
-# Add lines:
-# Package: *
-# Pin: release a=trusty-proposed
-# Pin-Priority: 400
+#!/bin/bash
+# RUN AS SUDO
 
 # Praise Mike
 echo "==============================="
@@ -21,9 +7,14 @@ echo "= PRAISE MIKE, GOD OF MINING. ="
 echo "==============================="
 sleep 5
 
+# Add graphics drivers repos
+echo "deb http://archive.ubuntu.com/ubuntu/ trusty-proposed restricted main multiverse universe" >> /etc/apt/sources.list
+echo "Package: *" >> /etc/apt/preferences.d/proposed-updates 
+echo "Pin: release a=trusty-proposed" >> /etc/apt/preferences.d/proposed-updates 
+echo "Pin-Priority: 400" >> /etc/apt/preferences.d/proposed-updates 
+
 # Add repositories and setup
-apt-get update
-apt-get upgrade -y
+apt-get update && apt-get upgrade -y
 add-apt-repository -y ppa:ethereum/ethereum
 add-apt-repository -y ppa:ethereum/ethereum-qt
 apt-get update
@@ -39,5 +30,5 @@ apt-get update && sudo apt-get upgrade -y
 reboot
 
 # Run
-# nohup geth --rpc --etherbase "0x753562eea69cd2d342a9c07edd49b9753dc0f01e" --genesis ~/genesis.json > ~/geth.log &
-# nohup sudo ethminer -G -t 4 > ~/ethminer.log &
+# nohup geth --rpc --etherbase "0xf47dF035c49e57DE2Ec27E32f0562BDeB4606ff7" > ~/geth.log &
+# sudo nohup ethminer -G -t 4 > ~/ethminer.log &
